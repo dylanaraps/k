7d708647e26936b6b8e1e1df761427f45cc86abf
Dylan Araps <dylan.araps@gmail.com>
2020-06-27 12:30:42 +0300

    docs: update

--- old/file.h
+++
@@ -1,4 +0,0 @@
-void cp_file(const char *src, const char *des);
-void cp_dir(const char *src, const char *des);
-void rm_dir(const char *src);
-void mkdir_p(const char *dir);
--- old/strlcpy.h
+++
@@ -1,3 +0,0 @@
-#include <stdlib.h>
-
-size_t strlcpy(char *dst, const char *src, size_t dsize);
