6d5b42621bbe48559f47231074991713658d3fe6
Dylan Araps <dylan.araps@gmail.com>
2020-11-05 09:35:27 +0200

    initial sha stuff

---
+++ include/sha256.h
@@ -0,0 +1,22 @@
+/**
+ * SPDX-License-Identifier: MIT
+ * Copyright (C) 2020 Dylan Araps
+ */
+#ifndef KISS_SHA256_H
+#define KISS_SHA256_H
+
+/**
+ * Use OpenSSL sha256 if available falling back to a standalone implementation.
+ * OpenSSL has ASM implementations of sha256 (which use SSSE3, AVX, whatever)
+ * for various architectures. LibreSSL only includes ASM for "major
+ * architectures", see https://github.com/q66/libressl-portable-asm.
+ */
+#ifdef USE_OPENSSL
+#include "openssl/sha.h"
+#else
+
+// todo
+
+#endif
+
+#endif
---
+++ src/sha256.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+#include "sha256.h"
+
+/**
+ * Fallback internal sha256 implementation.
+ * Modified to match OpenSSL's sha256 functions.
+ */
+#ifndef USE_OPENSSL
+
+// todo
+
+#endif
+
