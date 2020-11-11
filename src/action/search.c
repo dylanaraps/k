aecf8dc9d2a5669826aac01dff96a02aa9c8f57a
Dylan Araps <dylan.araps@gmail.com>
2020-11-11 07:39:45 +0200

    push

--- src/action/search.c
+++ src/action/search.c
@@ -11,62 +11,96 @@
 #include "buf.h"
 #include "action.h"
 
-int action_search(buf **buf, int argc, char *argv[]) {
-    buf_push_s(buf, getenv("KISS_PATH"));
-    buf_push_c(buf, ':');
-    buf_push_l(buf, "/var/db/kiss/installed", 22);
-
-    list repos;
-    glob_t g = {0};
-
-    if (list_init(&repos, 12) < 0) {
-        goto error;
+static int get_repositories(buf **buf) {
+    if (buf_push_s(buf, getenv("KISS_PATH")) == -ENOMEM) {
+        err("failed to allocate memory");
+        return -ENOMEM;
     }
 
-    for (char *t = strtok(*buf, ":"); t; t = strtok(NULL, ":")) {
+    buf_push_c(buf, ':');
+
+    if (buf_push_s(buf, getenv("KISS_ROOT")) == -ENOMEM) {
+        err("failed to allocate memory");
+        return -ENOMEM;
+    }
+
+    buf_rstrip(buf, '/');
+    buf_push_c(buf, '/');
+    buf_push_l(buf, "var/db/kiss/installed", 21);
+
+    return 0;
+}
+
+static int get_repo_list(list *repos, char *buf) {
+    if (list_init(repos, 12) < 0) {
+        err("failed to allocate memory");
+        return -ENOMEM;
+    }
+
+    for (char *t = strtok(buf, ":"); t; t = strtok(NULL, ":")) {
         if (t[0] != '/') {
             err("invalid path '%s' in KISS_PATH", t);
-            goto error;
+            return -1;
         }
 
-        list_push_b(&repos, t);
+        list_push_b(repos, t);
     }
 
-    size_t len = buf_len(*buf);
+    return 0;
+}
+
+int action_search(buf **buf, int argc, char *argv[]) {
+    int err = 0;
+
+    if ((err = get_repositories(buf)) < 0) {
+        err("failed to get repository list");
+        goto list_error;
+    }
+
+    list repos;
+
+    if ((err = get_repo_list(&repos, *buf)) < 0) {
+        err("failed to initialize repository list");
+        goto list_error;
+    }
+
+    glob_t g = { .gl_pathc = 0, };
 
     for (int i = 2; i < argc; i++) {
         size_t glob_pre = g.gl_pathc;
 
-        for (size_t j = 0; j < repos.len; j++) {
+        for (size_t j = 0, len = buf_len(*buf); j < repos.len; j++) {
             buf_push_s(buf, repos.arr[j]);
             buf_rstrip(buf, '/');
             buf_push_c(buf, '/');
             buf_push_s(buf, argv[i]);
             buf_push_c(buf, '/');
 
-            if (glob(*buf + len, g.gl_pathc ? GLOB_APPEND : 0, NULL, &g) < 0) {
-                goto error;
+            err = glob(*buf + len, g.gl_pathc ? GLOB_APPEND : 0, NULL, &g);
+
+            if (err < 0) {
+                err("glob encountered error with query '%s'", *buf + len);
+                goto glob_error;
             }
 
             buf_set_len(*buf, len);
         }
 
-        if (g.gl_pathc - glob_pre == 0) {
+        if ((g.gl_pathc - glob_pre) == 0) {
             err("no search results for '%s'", argv[i]);
-            goto error;
+            err = -1;
+            goto glob_error;
         }
     }
 
     for (size_t i = 0; i < g.gl_pathc; i++) {
-        printf("%s\n", g.gl_pathv[i]);
+        puts(g.gl_pathv[i]);
     }
 
-    list_free(&repos);
+glob_error:
     globfree(&g);
-    return 0;
-error:
+list_error:
     list_free(&repos);
-    globfree(&g);
-    return -1;
+    return err;
 }
 
