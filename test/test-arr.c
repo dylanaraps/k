4338ec2f636976826f8e3d818b9a0a268d1b4a6b
Dylan Araps <dylan.araps@gmail.com>
2020-11-11 11:25:19 +0200

    push

---
+++ test/test-arr.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "arr.h"
+#include "test.h"
+
+static const char *fruit[] = {
+    "apple",
+    "orange",
+    "pear",
+    "banana",
+    "peach",
+    "lime"
+};
+
+static int compare(void const *a, void const *b) {
+    return strcmp(*(const char **) a, *(const char **) b);
+}
+
+int main(int argc, char *argv[]) {
+    (void) argc;
+    (void) argv;
+
+    test_begin(__FILE__);
+
+    char **list = arr_alloc(0, 4); {
+        test(list);
+        test(arr_cap(list) == 4);
+        test(arr_len(list) == 0);
+    }
+
+    for (size_t i = 0; i < sizeof(fruit) / sizeof(fruit[0]); i++) {
+        char *s = strdup(fruit[i]);
+
+        arr_push_b(list, s); {
+            test(arr_len(list) == i + 1);
+            test(strcmp(list[0], "apple") == 0);
+        }
+    }
+
+    arr_sort(list, compare); {
+        test(arr_cap(list) == 6);
+        test(arr_len(list) == 6);
+        test(strcmp(list[0], "apple") == 0);
+        test(strcmp(list[1], "banana") == 0);
+        test(strcmp(list[2], "lime") == 0);
+        test(strcmp(list[3], "orange") == 0);
+        test(strcmp(list[4], "peach") == 0);
+        test(strcmp(list[5], "pear") == 0);
+    }
+
+    free(list[arr_len(list) - 1]);
+    arr_drop_b(list); {
+        test(arr_cap(list) == 6);
+        test(arr_len(list) == 5);
+        test(strcmp(list[0], "apple") == 0);
+        test(strcmp(list[1], "banana") == 0);
+        test(strcmp(list[2], "lime") == 0);
+        test(strcmp(list[3], "orange") == 0);
+        test(strcmp(list[4], "peach") == 0);
+    }
+
+    free(list[arr_len(list) - 1]);
+    arr_drop_b(list); {
+        test(arr_cap(list) == 6);
+        test(arr_len(list) == 4);
+        test(strcmp(list[0], "apple") == 0);
+        test(strcmp(list[1], "banana") == 0);
+        test(strcmp(list[2], "lime") == 0);
+        test(strcmp(list[3], "orange") == 0);
+    }
+
+    for (size_t i = 0; i < arr_len(list); i++) {
+        free(list[i]);
+    }
+    arr_free(list);
+
+    return test_finish();
+}
+
