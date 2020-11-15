9ce11807e029d3438a5a4db277645c22dfc51cf0
Dylan Araps <dylan.araps@gmail.com>
2020-11-15 21:05:26 +0200

    push

--- src/action/search.c
+++ src/action/search.c
@@ -13,36 +13,26 @@
 #include "action.h"
 
 int action_search(struct state *s) {
-    int err = 0;
     glob_t g = { .gl_pathc = 0, };
 
     for (size_t i = 0; i < arr_len(s->pkgs); i++) {
         size_t glob_pre = g.gl_pathc;
 
-        for (size_t j = 0, l = buf_len(s->mem); j < arr_len(s->repos); j++) {
-            buf_push_s(&s->mem, s->repos[j]->path);
-            buf_rstrip(&s->mem, '/');
-            buf_push_c(&s->mem, '/');
-            buf_push_s(&s->mem, s->pkgs[i]->name);
-            buf_push_c(&s->mem, '/');
+        for (size_t j = 0; j < arr_len(s->repos); j++) {
+            buf_set_len(s->mem, 0);
+            buf_printf(&s->mem, "%s/%s/",
+                s->repos[j]->path, s->pkgs[i]->name);
 
-            switch (glob(s->mem + l, g.gl_pathc ? GLOB_APPEND : 0, NULL, &g)) {
+            switch (glob(s->mem, g.gl_pathc ? GLOB_APPEND : 0, NULL, &g)) {
                 case GLOB_NOSPACE:
                 case GLOB_ABORTED:
-                    err("glob encountered error with query '%s'", s->mem + l);
+                    err("glob encountered error with query '%s'", s->mem);
                     goto glob_error;
-
-                case GLOB_NOMATCH:
-                default:
-                    break;
             }
-
-            buf_set_len(s->mem, l);
         }
 
         if ((g.gl_pathc - glob_pre) == 0) {
             err("no search results for '%s'", s->pkgs[i]->name);
-            err = -1;
             goto glob_error;
         }
     }
@@ -51,8 +41,11 @@ int action_search(struct state *s) {
         puts(g.gl_pathv[i]);
     }
 
+    globfree(&g);
+    return 0;
+
 glob_error:
     globfree(&g);
-    return err;
+    return -1;
 }
 
